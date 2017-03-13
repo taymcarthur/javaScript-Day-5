9fb87e3bdeed46e8a975ba920ef3424ec8cb46c3
Taylor McArthur <taydm.erek@gmail.com>
2017-03-13 14:24:22 -0700

    made mini revisions to code

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-
 $(document).ready(function(){
-	$.getJSON('http://json-data.hereokuapp.com/forms', function(formData){
-		//ALL CODE GOES HERE
-		//FORMDATA VARIABLE INCLUDES ALL OF THE FORM INFORMATION YOU NEED
-//How to insert variable into string: with nopostrophies `${asdk}`
+  $.getJSON('http://json-data.herokuapp.com/forms', function(formData){
+    //ALL CODE GOES HERE
+    //FORMDATA VARIABLE INCLUDES ALL OF THE FORM INFORMATION YOU NEED
+    //How to insert variable into string: with nopostrophies `${asdk}
 
-		var formHTML = ''
+    var formHTML = ''
 
-		formData.forEach(function(field){
-			formHTML += `<i class="fa${field.icon} input type=${field.type} placeholder=${field.label} id=${field.id} />`
-		})
+    formData.forEach(function(field){
+      formHTML += `<i class="fa ${field.icon}"><input type="${field.type}" placeholder="${field.label}" id="${field.id}" />`
+    })
 
-		$(".form").html(formHTML)
+    $(".form").html(formHTML)
+  })
 
 
-
-
-	})
 })
